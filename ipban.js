040c2772f07558822a8b2c85bc80fa3ed46a4407
Matt Novoselov <59065228+matt-novoselov@users.noreply.github.com>
2023-01-15 03:42:19 +0300

    Add files via upload

--- ipban.js
+++ ipban.js
@@ -1,8 +1,7 @@
 fetch('https://ipinfo.io/json').then((res) => {
   res.json().then((data) => {         
     if(data.country === "RU"){
-      window.location.href = 'https://blog.hubspot.com/website/ip-redirect';
-      window.alert(data.country);
+      window.location.href = 'https://crazyleaker.com/stop-war';
     }
   });
 });
